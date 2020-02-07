7a21335df2230454667e9eccdac65aa313c16e56
VarunDev7777 <dudevarun7777@gmail.com>
2020-02-07 22:56:22 +0530

    page added

--- controllers/landing.js
+++ controllers/landing.js
@@ -9,4 +9,12 @@ Router.get('/sell-car', (req, res) => {
     res.render('sell_car');
 })
 
+Router.get('/buy-car', (req, res) => {
+    res.render('buy_car')
+})
+
+Router.get('/car-submit', (req, res) => {
+    res.render('sell_form')
+})
+
 module.exports = Router;
