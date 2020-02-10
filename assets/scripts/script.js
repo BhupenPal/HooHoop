d2fbbfc24039a7b46cc62e3a3b7381a76ad058b6
VarunDev7777 <dudevarun7777@gmail.com>
2020-02-11 00:54:56 +0530

    sell car added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,23 +8,24 @@ var i=0
 function sld_unsld(j){
     let x = document.getElementsByClassName("select")
     let y = document.getElementsByClassName("opt_enclose")
-    let z = document.getElementsByClassName("arr_1")
+    let z = document.getElementsByClassName("select_opener")[j]
+    let arr_change = z.getElementsByClassName("arr")
 
     if(i%2==0){
         x[j].style.border = "#43b4ff 2px solid"
         y[j].style.display = "block"
-        for(p=0;p<2;p++){
-            z[p].style.backgroundColor = "#43b4ff"
-        }
+        arr_change[0].style.backgroundColor = "#43b4ff"
+        arr_change[0].style.transform = "rotate(-45deg) translateX(1px) translateY(2px)"
+        arr_change[1].style.backgroundColor = "#43b4ff"
+        arr_change[1].style.transform = "rotate(45deg) translateX(-1.5px) translateY(2.5px)"
         i++
     }
 
     else{
         x[j].removeAttribute("style")
         y[j].removeAttribute("style")
-        for(p=0;p<2;p++){
-            z[p].removeAttribute("style")
-        }
+        arr_change[0].removeAttribute("style")
+        arr_change[1].removeAttribute("style")
         i--
     }
 }
