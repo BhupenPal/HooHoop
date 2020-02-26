3f5c33cf2ae39b481c9b9358202fa45fef60810a
BhupenPal <bhupen16pal@gmail.com>
2020-02-26 18:59:10 +0530

    get ajax

---
+++ models/testDrive.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/HooHoop", {
+  useNewUrlParser: !0,
+  useUnifiedTopology: !0,
+  useCreateIndex: 1
+});
+
+mongoose.connection.on("connected", () =>
+  console.log("Mongoose Test Drive Model Intitialized!!!!")
+);
+
+const Schema = mongoose.Schema;
+const TestDrive = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: String,
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNum: {
+    type: Number,
+    required: true
+  },
+  customerID: String,
+  vehicleID: String
+});
+
+module.exports = mongoose.model("Test Drive", TestDrive);
