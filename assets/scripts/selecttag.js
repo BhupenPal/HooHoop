6bf7cfd2e65df55ed3e7ec1375ee485bb6268f68
VarunDev7777 <dudevarun7777@gmail.com>
2020-03-10 12:47:52 +0530

    select tag partially completed

--- assets/scripts/selecttag.js
+++ assets/scripts/selecttag.js
@@ -1,46 +1,56 @@
-function sld_unsld(j) {
-    let x = document.getElementsByClassName("select");
-    let y = document.getElementsByClassName("opt_enclose");
-    let z = document.getElementsByClassName("select_opener")[j];
-    let arr_change = z.getElementsByClassName("arr");
+let x = document.getElementsByClassName("select")
+let y = document.getElementsByClassName("opt_enclose");
 
-    console.log(i)
-     
-    window.addEventListener("click",function(eve){
-      let isclicked = x[j].contains(eve.target)
-      
-      if(!isclicked){
-        x[j].removeAttribute("style");
-        y[j].removeAttribute("style");
-        arr_change[0].removeAttribute("style");
-        arr_change[1].removeAttribute("style");
-        i = 0;
-      }
-      
-      else{
-        if(i==0){
-        x[j].style.border = "#43b4ff 2px solid";
-        y[j].style.display = "block";
-        arr_change[0].style.backgroundColor = "#43b4ff";
-        arr_change[0].style.transform =
-          "rotate(-45deg) translate3d(12px,12px,0)";
-        arr_change[1].style.backgroundColor = "#43b4ff";
-        arr_change[1].style.transform =
-          "rotate(45deg) translate3d(1.5px,-1px,0)";  
-          i=1
-          console.log(i)
-          
-        } 
-      else{
-        x[j].removeAttribute("style");
-        y[j].removeAttribute("style");
-        arr_change[0].removeAttribute("style");
-        arr_change[1].removeAttribute("style");
-        i = 0;
-        console.log(i)
-      }
-    }
+var i = "closed";
+  
+  for(p==0;p<x.length;p++){
+    x[p].addEventListener("click",function(){
+      console.log(this)
     })
+  }
+
+window.addEventListener("click",function sld_unsld(){
+  for(var k=0;k<x.length;k++){
+    let isclicked = x[k].contains(event.target)
+    if(!isclicked){
+      x[k].removeAttribute("style")
+      y[k].removeAttribute("style")
+      let z = document.getElementsByClassName("select_opener")[k];
+      let arr_change = z.getElementsByClassName("arr");
+      arr_change[0].removeAttribute("style");
+      arr_change[1].removeAttribute("style");
+      i="closed"
+    }
+    else{
+      callselect(k)
+    }
+  }
+})
+  
+  function callselect(j){
+
+      let z = document.getElementsByClassName("select_opener")[j];
+      let arr_change = z.getElementsByClassName("arr");
+    console.log(i)
+    if(i=="closed"){
+      x[j].style.border = "#43b4ff 2px solid";
+      y[j].style.display = "block";
+      arr_change[0].style.backgroundColor = "#43b4ff";
+      arr_change[0].style.transform =
+        "rotate(-45deg) translate3d(12px,12px,0)";
+      arr_change[1].style.backgroundColor = "#43b4ff";
+      arr_change[1].style.transform =
+        "rotate(45deg) translate3d(1.5px,-1px,0)";  
+        i="opened";
+        console.log(i)
+      } 
+    else{
+      x[j].removeAttribute("style");
+      y[j].removeAttribute("style");
+      arr_change[0].removeAttribute("style");
+      arr_change[1].removeAttribute("style");
+      i="closed"
+    }
   }
   
   function opt_select(k, j) {
