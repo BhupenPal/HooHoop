7af697f428d074a79377802e64e336c3870d3757
BhupenPal <bhupen16pal@gmail.com>
2020-03-12 17:17:58 +0530

    Dashboard Vanish

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,12 +1,16 @@
-let dashoption = document.getElementsByClassName("fet_det")
-let sopt = document.getElementsByClassName("sopt")
+let dashoption = document.getElementsByClassName("fet_det");
+let sopt = document.getElementsByClassName("sopt");
 
-for(var m=0;m<sopt.length-1;m++){
-  sopt[m].addEventListener("click",function(){
-    for(var n=0;n<sopt.length-1;n++){
-      sopt[n].classList.remove("selected_sopt")
+for (var m = 0; m < sopt.length - 1; m++) {
+  sopt[m].addEventListener("click", function() {
+    for (var n = 0; n < sopt.length - 1; n++) {
+      dashoption[n].classList.add("vanish");
+      if (sopt[n].classList.contains("selected_sopt")) {
+        sopt[n].classList.remove("selected_sopt");
+        dashoption[n + 1].classList.remove("vanish");
+      }
     }
-    this.classList.add("selected_sopt")
-    console.log(this.classList.item(0))
-  })
+    this.classList.add("selected_sopt");
+    console.log(this.classList.item(0));
+  });
 }
