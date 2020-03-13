c2f4b2485fe993edddc19934ce292ac98e599912
VarunDev7777 <dudevarun7777@gmail.com>
2020-03-13 14:45:21 +0530

    dashboard option added

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -14,3 +14,20 @@ for(let m=0; m<sopt.length - 1; m++){
     this.click()
   })
 }
+
+let clienttable = document.getElementsByClassName("client_table")
+let clientmopt = document.getElementsByClassName("Paneopt")
+
+for(var s=0; s<clientmopt.length;s++){
+  clientmopt[s].addEventListener("click", function(){
+    for(let n=0;n<clientmopt.length;n++){
+      clienttable[n].classList.add("vanish")
+      if(clientmopt[n].classList.contains("selected_paneopt")){
+        clientmopt[n].classList.remove("selected_paneopt")
+        clienttable[n].classList.remove("vanish")
+      }
+    }
+    this.classList.add("selected_paneopt")
+    this.click()
+  })
+}
